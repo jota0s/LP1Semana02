02d79f4622e28596da80dc17e8b2ff1bd6c93e06
jota0s <joaosilvaa.work@gmail.com>
2023-03-09 21:57:25 +0000

    Create a variable to convert int to string

---
+++ ManipulaVar/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ManipulaVar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+           int v= int.Parse(args[0]);
+           v++;
+           Console.WriteLine(v);
+           
+           
+           
+        }
+    }
+}
