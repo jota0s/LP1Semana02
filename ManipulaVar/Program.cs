631bc198f0dfa9f4cec8a5ddf9a5be75a68811e5
jota0s <joaosilvaa.work@gmail.com>
2023-03-09 22:36:29 +0000

    add coments

--- ManipulaVar/Program.cs
+++ ManipulaVar/Program.cs
@@ -6,17 +6,16 @@ namespace ManipulaVar
     {
         static void Main(string[] args)
         {
-           
+           //convert args to a int an print v + 1 
            int v= int.Parse(args[0]);
            v++;
            Console.WriteLine(v);
            
+           ////convert args to a int an print x - 1 
            int x= int.Parse(args[0]);
            --x;
            Console.WriteLine(x);
-           
 
-           
            
            
         }
