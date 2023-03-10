47b4f0d786fae90ed9f3a709ed8ee793351c8d66
jota0s <joaosilvaa.work@gmail.com>
2023-03-10 13:33:12 +0000

    create the variables and the formula of V and Sa

---
+++ PerguntaValores/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PerguntaValores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            
+            Console.WriteLine("enter value of (h)");
+            float h =float.Parse(Console.ReadLine());
+
+             Console.WriteLine("enter value of (r)");
+            float r =float.Parse(Console.ReadLine());
+            
+            double Pi = 3.1415926;
+
+           double V = Pi*r*r*h;
+           Console.WriteLine(V);
+
+           double S = 2*Pi*r*(h + r);
+           Console.WriteLine(S);
+
+ 
+
+
+
+
+        }
+    }
+}
