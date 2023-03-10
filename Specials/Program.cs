0e198991859f19668b6022c02ee9760ee6b2f763
jota0s <joaosilvaa.work@gmail.com>
2023-03-10 14:47:44 +0000

    create int variable short and max

---
+++ Specials/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Specials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          int i = short.MaxValue;
+          Console.WriteLine(i);
+          Console.WriteLine((short)(i +1));
+
+          float t=float.MaxValue;
+          Console.WriteLine(t);
+          Console.WriteLine((float)(i +1));
+
+        }
+    }
+}
