42c64a09d0c9ed99f784f3450cb4de2c418b1b45
jota0s <joaosilvaa.work@gmail.com>
2023-03-10 13:47:48 +0000

    add coments

--- PerguntaValores/Program.cs
+++ PerguntaValores/Program.cs
@@ -6,20 +6,23 @@ namespace PerguntaValores
     {
         static void Main(string[] args)
         {   
-            
+            //read the input str and convert to float
             Console.WriteLine("enter value of (h)");
             float h =float.Parse(Console.ReadLine());
 
-             Console.WriteLine("enter value of (r)");
+            //read the input str and convert to float
+            Console.WriteLine("enter value of (r)");
             float r =float.Parse(Console.ReadLine());
             
+            //create the variable Pi
             double Pi = 3.1415926;
 
-           double V = Pi*r*r*h;
-           Console.WriteLine(V);
-
-           double S = 2*Pi*r*(h + r);
-           Console.WriteLine(S);
+            //formula to calculate V
+            double V = Pi*r*r*h;
+            Console.WriteLine(V);
+            //formula to calculate Sa
+            double S = 2*Pi*r*(h + r);
+            Console.WriteLine(S);
 
  
 
