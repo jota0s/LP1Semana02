2c6a3ecf0077c5411912dbaf4a2487431e27f481
Joao Silva <joaosilvaa.work@gmail.com>
2025-02-26 17:54:56 +0000

    add SmoothOperator

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SmootOperator
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {   
+
+            //ask for a number and save the input
+            Console.Write("Insere um número inteiro: ");
+            string p = Console.ReadLine();
+            int n = sbyte.Parse(p);
+
+            //print the input with increment and decrement 
+            Console.WriteLine(--n);
+            Console.WriteLine(++n);
+
+
+        }
+    }
+}
