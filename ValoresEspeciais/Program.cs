63bf89bbc2c6553813ab588ba046ff8ff3a37be7
Joao Silva <joaosilvaa.work@gmail.com>
2025-02-28 01:54:48 +0000

    add variables max and min value

---
+++ ValoresEspeciais/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ValoresEspeciais
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            sbyte sb = sbyte.MaxValue;
+            sbyte sb1 = sbyte.MinValue;
+            double db = double.MaxValue;
+            double db1 = double.MinValue;
+            ulong ul = ulong.MaxValue;
+            ulong ul1 = ulong.MinValue;
+            int i = int.MaxValue;
+            int i1 = int.MinValue;
+            uint ui = uint.MaxValue;
+            uint ui1 = uint.MinValue;
+            byte b = byte.MaxValue;
+            byte b1 = byte.MinValue;
+            short s = short.MaxValue;
+            short s1 = short.MinValue;
+            long l = long.MaxValue;
+            long l1 = long.MinValue;
+            float f = float.MaxValue;
+            float f1 = float.MinValue;
+
+             Console.WriteLine(sb);
+            Console.WriteLine(sb1);
+            Console.WriteLine(db);
+            Console.WriteLine(db1);
+            Console.WriteLine(ul);
+            Console.WriteLine(ul1);
+            Console.WriteLine(i);
+            Console.WriteLine(i1);
+            Console.WriteLine(ui);
+            Console.WriteLine(ui1);
+            Console.WriteLine(b);
+            Console.WriteLine(b1);
+            Console.WriteLine(s);
+            Console.WriteLine(s1);
+            Console.WriteLine(l);
+            Console.WriteLine(l1);
+            Console.WriteLine(f);
+            Console.WriteLine(f1);
+
+
+        }
+    }
+}
