3cf21fdacaa27e8f0ac4d9e513a8e04463c79928
Joao Silva <joaosilvaa.work@gmail.com>
2025-02-28 01:29:51 +0000

    create VariousOperators

---
+++ VariousOperators/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace VariousOperators
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            //ask for a number and save the input
+            Console.Write("Insere um número inteiro-não negativo: ");
+            string p = Console.ReadLine();
+            int n = sbyte.Parse(p);
+
+                             
+            Console.WriteLine(n/2);
+            Console.WriteLine(n <<3);
+            Console.WriteLine(n <<3);
+            Console.WriteLine(n ^6);
+            Console.WriteLine(n >10);
+            
+        }
+    }   
+}
